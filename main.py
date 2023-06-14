742b8bf4fe762f54cc54348cdeb678c929aeaf41
Akhilesh-Varma <akhileshvarmab@gmail.com>
2023-06-14 19:49:44 -0300

    tell me ..

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import os
 from constants import openai_key
 from langchain.llms import OpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
 
 import streamlit as st
 
@@ -13,10 +15,20 @@ st.title("LinkedIn Data Science Job scan")
 
 input_text = st.text_input("Search for any job related information ")
 
+# Prompt template
+first_import_prompt = PromptTemplate(
+    input_variables = ['name'],
+    template = "Tell me about job {name}"
+)
+
 # OpenAI LLM
 
 llm = OpenAI(temperature = 0.8)
 
+chain = LLMChain(llm = llm, prompt = first_import_prompt, verbose = True)
+
+
+
 if input_text:
-    st.write(llm(input_text))
+    st.write(chain.run(input_text))
 
